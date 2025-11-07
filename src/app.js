71caa7feee46f07fcb985c278fa9105c570b8ca8
Jean Alexandre Vergara <jean.alexandre@usm.cl>
2025-11-07 19:38:19 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
 const app = express();
 const simulacionRoutes = require("./simulacion");
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// 🔹 IMPORTANTE: Usa "Public" con mayúscula
-app.use(express.static(path.join(__dirname, "Public")));
-
-app.use("/api", simulacionRoutes);
-
+// Servir el HTML principal
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "Public", "Prototipo_Sistema_Grupo15.html"));
 });
+
+// Rutas de la API
+app.use("/api", simulacionRoutes);
 
 module.exports = app;
