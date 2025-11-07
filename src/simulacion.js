00e38f340b95e59bcce2eaf3783e69cf0e1680ac
Jean Alexandre Vergara <jean.alexandre@usm.cl>
2025-11-07 18:15:03 -0300

    Update simulacion.js

--- src/simulacion.js
+++ src/simulacion.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const db = require("../db");
+const db = require("./db");
 const { calcularScoring } = require("../scoring");
 
 router.post("/evaluar", async (req, res) => {
@@ -26,3 +26,4 @@ router.post("/evaluar", async (req, res) => {
 });
 
 module.exports = router;
+
