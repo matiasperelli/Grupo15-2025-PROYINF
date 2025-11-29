1dff3dec26f5b48b416b53153ade8a0264389de6
Vicente-beiza <vicente.beiza@usm.cl>
2025-11-28 23:46:39 -0300

    Integrate desembolsoRouter into firma_routes

--- src/Firma/firma_routes.js
+++ src/Firma/firma_routes.js
@@ -1,5 +1,8 @@
 import express from "express";
+import desembolsoRouter from "./desembolso.routes.js";
 import { postFirma } from "./firma.controller.js";
+
+app.use("/api", desembolsoRouter);
 
 const router = express.Router();
 
